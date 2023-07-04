2d96b67f9ba3841ce744e2552b081e592b93db33
Crowdbotics <team@crowdbotics.com>
2023-07-04 17:29:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tkf-notes-41900",
+  url: "https://tkf-notes-41900.botics.co",
+  api: "https://tkf-notes-41900.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
